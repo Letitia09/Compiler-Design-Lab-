591eb5db1b6c62bc27cab9d8a1603e47009755a0
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2022-04-19 11:43:45 +0530

    Create Recursive Descent Parser.c

---
+++ Recursive Descent Parser3/Recursive Descent Parser.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+int i = 0, f = 0;
+char str[30];
+
+int S();
+int E();
+int Eprime();
+
+int S()
+{
+    if (str[i] == '(')
+    {
+        printf("S->(E)");
+        i++;
+        E();
+        if(str[i]==')'){
+            i++;
+        }
+    }
+    else if ((str[i] == 'b')){
+        printf("S->b");
+        i++;}
+    else{
+        f=1;
+    }
+    return 0;
+}
+
+int E()
+{
+    printf("E->SE'");
+    S();
+    Eprime();
+    return 0;
+}
+
+int Eprime()
+{
+    if (str[i] == ',')
+    {
+        printf("E'->,SE'");
+        i++;
+        S();
+        Eprime();
+    }
+    else if ((str[i] == '^')||(str[i]==')')){
+        printf("E'->^");
+    }
+    return 0;
+}
+
+
+
+int main()
+{
+    int len;
+    scanf("%s", str);
+    len = strlen(str);
+    str[len] = '$';
+    S();
+    if ((str[i] == '$') && (f == 0))
+        printf("string sucessfully parsed!");
+    else
+        printf("syntax Error!");
+    return 0;
+}
